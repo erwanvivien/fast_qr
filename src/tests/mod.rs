471c06983c84cbf3095504de2ad1b0b3a2258f56
erwan.vivien <erwan.vivien@epita.fr>
2021-08-27 22:33:00 +0200

    [BUG] Fix crates path in tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,31 +1,31 @@
 #[test]
 fn version_size_01() {
-    let mat = crate::create_matrix_from_version(1);
+    let mat = crate::default::create_matrix_from_version(1);
     let length = mat.len();
     assert_eq!(length, 21);
 }
 #[test]
 fn version_size_03() {
-    let mat = crate::create_matrix_from_version(3);
+    let mat = crate::default::create_matrix_from_version(3);
     let length = mat.len();
     assert_eq!(length, 29);
 }
 #[test]
 fn version_size_22() {
-    let mat = crate::create_matrix_from_version(22);
+    let mat = crate::default::create_matrix_from_version(22);
     let length = mat.len();
     assert_eq!(length, 105);
 }
 #[test]
 fn version_size_40() {
-    let mat = crate::create_matrix_from_version(40);
+    let mat = crate::default::create_matrix_from_version(40);
     let length = mat.len();
     assert_eq!(length, 177);
 }
 
 #[test]
 fn position_pattern_03() {
-    let mat = crate::create_matrix_from_version(3);
+    let mat = crate::default::create_matrix_from_version(3);
     // Top line
     assert!(mat[0][0]);
     assert!(mat[0][1]);
@@ -75,7 +75,7 @@ fn position_pattern_03() {
 /// Checks it's empty
 #[test]
 fn position_pattern_01() {
-    let mat = crate::create_matrix_from_version(1);
+    let mat = crate::default::create_matrix_from_version(1);
 
     for i in 7..21 {
         for j in 7..21 {
