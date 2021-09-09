32c1a75bda9f0750a6111c3301bc8e55910bd74a
erwan.vivien <erwan.vivien@epita.fr>
2021-09-09 14:36:52 +0200

    [TESTS] Fix tests because of new archi & added one for Data codewords

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -89,6 +89,28 @@ fn position_pattern_01() {
 }
 
 #[test]
+fn data_encoding_hello_world() {
+    let version = 1;
+    const QUALITY: crate::vecl::ECL = crate::vecl::ECL::Q;
+    let string_to_encode: String = String::from("HELLO WORLD");
+
+    let vec = crate::alphanum::encode_alphanum(string_to_encode, version, QUALITY);
+    let tmp1 = vec.unwrap().to_vec();
+
+    let mut bitstorage = crate::bitstorage::BitStorage::new();
+    bitstorage.push_binstr("00100000010110110000101101111000110100010111001011011100010011010100001101000000111011000001000111101100");
+
+    assert_eq!(
+        tmp1,
+        [
+            0b00100000, 0b01011011, 0b00001011, 0b01111000, 0b11010001, 0b01110010, 0b11011100,
+            0b01001101, 0b01000011, 0b01000000, 0b11101100, 0b00010001, 0b11101100
+        ]
+    );
+    assert_eq!(tmp1, bitstorage.to_vec());
+    //00100000,01011011,00001011,01111000,11010001,01110010,11011100,01001101,01000011,01000000,11101100,00010001,11101100
+}
+
 #[test]
 fn generator_polynomial_7() {
     let poly = crate::polynomials::GENERATOR_POLYNOMIALS[7];
@@ -411,7 +433,7 @@ fn error_code_computation_05_13() {
     let string_to_encode: String = String::from("HELLO MY NAME IS ERWAN");
 
     let vec = crate::alphanum::encode_alphanum(string_to_encode, version, QUALITY);
-    let tmp1 = crate::helpers::binarystring_to_binary(&vec);
+    let tmp1 = vec.unwrap().to_vec();
 
     let tmp2 = crate::polynomials::GENERATOR_POLYNOMIALS[13];
 
@@ -428,7 +450,7 @@ fn error_code_computation_05_15() {
     let string_to_encode: String = String::from("HELLO MY NAME IS ERWAN");
 
     let vec = crate::alphanum::encode_alphanum(string_to_encode, version, QUALITY);
-    let tmp1 = crate::helpers::binarystring_to_binary(&vec);
+    let tmp1 = vec.unwrap().to_vec();
 
     let tmp2 = crate::polynomials::GENERATOR_POLYNOMIALS[15];
 
@@ -445,7 +467,7 @@ fn error_code_computation_05_28() {
     let string_to_encode: String = String::from("HELLO MY NAME IS ERWAN");
 
     let vec = crate::alphanum::encode_alphanum(string_to_encode, version, QUALITY);
-    let tmp1 = crate::helpers::binarystring_to_binary(&vec);
+    let tmp1 = vec.unwrap().to_vec();
 
     let tmp2 = crate::polynomials::GENERATOR_POLYNOMIALS[28];
 
@@ -467,7 +489,7 @@ fn error_code_computation_05_18() {
     let string_to_encode: String = String::from("HELLO MY NAME IS ERWAN");
 
     let vec = crate::alphanum::encode_alphanum(string_to_encode, version, QUALITY);
-    let tmp1 = crate::helpers::binarystring_to_binary(&vec);
+    let tmp1 = vec.unwrap().to_vec();
 
     let tmp2 = crate::polynomials::GENERATOR_POLYNOMIALS[crate::vecl::ecc_to_ect(QUALITY, version)];
 
