c2b6a5d6c1952e0ab36912c984d43919d01769c2
erwan.vivien <erwan.vivien@epita.fr>
2021-09-21 13:23:09 +0200

    [OPTI] Reduce loop length in data mask

--- src/datamasking.rs
+++ src/datamasking.rs
@@ -3,8 +3,8 @@
 /// Mask function nb°0
 fn mask_0(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
-            if !mat_full[row][column] && (row + column) % 2 == 0 {
+        for column in ((row & 1)..mat[0].len()).step_by(2) {
+            if !mat_full[row][column] {
                 mat[row][column] = !mat[row][column];
             }
         }
@@ -13,9 +13,9 @@ fn mask_0(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 
 /// Mask function nb°1
 fn mask_1(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
-    for row in 0..mat.len() {
+    for row in (0..mat.len()).step_by(2) {
         for column in 0..mat[0].len() {
-            if !mat_full[row][column] && row % 2 == 0 {
+            if !mat_full[row][column] {
                 mat[row][column] = !mat[row][column];
             }
         }
@@ -25,8 +25,8 @@ fn mask_1(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 /// Mask function nb°2
 fn mask_2(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
-            if !mat_full[row][column] && (column) % 3 == 0 {
+        for column in (0..mat[0].len()).step_by(3) {
+            if !mat_full[row][column] {
                 mat[row][column] = !mat[row][column];
             }
         }
@@ -36,8 +36,8 @@ fn mask_2(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 /// Mask function nb°3
 fn mask_3(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
-            if !mat_full[row][column] && (row + column) % 3 == 0 {
+        for column in ((3 - row % 3) % 3..mat[0].len()).step_by(3) {
+            if !mat_full[row][column] {
                 mat[row][column] = !mat[row][column];
             }
         }
@@ -46,10 +46,13 @@ fn mask_3(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 
 /// Mask function nb°4
 fn mask_4(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
+    let width = mat[0].len();
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
-            if !mat_full[row][column] && (row / 2 + column / 3) % 2 == 0 {
-                mat[row][column] = !mat[row][column];
+        for column in (((row / 2) % 2) * 3..width).step_by(6) {
+            for i in column..std::cmp::min(width, column + 3) {
+                if !mat_full[row][i] {
+                    mat[row][i] = !mat[row][i];
+                }
             }
         }
     }
@@ -58,10 +61,16 @@ fn mask_4(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 /// Mask function nb°5
 fn mask_5(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
+        for column in row..mat[0].len() {
             let row_col = row * column;
-            if !mat_full[row][column] && (row_col % 2) + (row_col % 3) == 0 {
+            let ok: bool = (row_col % 2) + (row_col % 3) == 0;
+
+            if !mat_full[row][column] && ok {
                 mat[row][column] = !mat[row][column];
+            }
+
+            if !mat_full[column][row] && ok && column != row {
+                mat[column][row] = !mat[column][row];
             }
         }
     }
@@ -70,10 +79,16 @@ fn mask_5(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
 /// Mask function nb°6
 fn mask_6(mat: &mut Vec<Vec<bool>>, mat_full: &Vec<Vec<bool>>) {
     for row in 0..mat.len() {
-        for column in 0..mat[0].len() {
+        for column in row..mat[0].len() {
             let row_col = row * column;
-            if !mat_full[row][column] && ((row_col % 2) + (row_col % 3)) % 2 == 0 {
+            let ok: bool = ((row_col % 2) + (row_col % 3)) % 2 == 0;
+
+            if !mat_full[row][column] && ok {
                 mat[row][column] = !mat[row][column];
+            }
+
+            if !mat_full[column][row] && ok && column != row {
+                mat[column][row] = !mat[column][row];
             }
         }
     }
