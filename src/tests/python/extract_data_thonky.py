f0f483e677a110d98e4948af554a12e26933cc90
erwan.vivien <erwan.vivien@epita.fr>
2021-10-13 17:54:57 +0200

    [OPTI] now print True as true and False as false

--- src/tests/python/extract_data_thonky.py
+++ src/tests/python/extract_data_thonky.py
@@ -5,6 +5,7 @@ import time
 import os
 import sys
 
+
 class RedirectStdStreams(object):
     def __init__(self, stdout=None, stderr=None):
         self._stdout = stdout or sys.stdout
@@ -12,17 +13,20 @@ class RedirectStdStreams(object):
 
     def __enter__(self):
         self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
-        self.old_stdout.flush(); self.old_stderr.flush()
+        self.old_stdout.flush()
+        self.old_stderr.flush()
         sys.stdout, sys.stderr = self._stdout, self._stderr
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self._stdout.flush(); self._stderr.flush()
+        self._stdout.flush()
+        self._stderr.flush()
         sys.stdout = self.old_stdout
         sys.stderr = self.old_stderr
 
-text="8675309"
+
+text = "HELLO WORLD"
 quality = 'q'
-url = f"https://www.thonky.com/qr-encoder/encoder.php?s=${text}&ec={quality}"
+url = f"https://www.thonky.com/qr-encoder/encoder.php?s=${text.replace(' ', '%20')}&ec={quality}"
 
 devnull = open(os.devnull, 'w')
 with RedirectStdStreams(stdout=devnull, stderr=devnull):
@@ -61,8 +65,7 @@ for i in range(qr_code_side):
 
 
 print(f"{text = }")
-print(qr)
-
+print(str(qr).replace("True", "true").replace("False", "false"))
 
 
 time.sleep(0.1)
