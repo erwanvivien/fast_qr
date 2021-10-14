a4bb7e48d2aaa6a4ab06f52dd28a9ea009a80eb2
erwan.vivien <erwan.vivien@epita.fr>
2021-10-14 23:39:06 +0200

    [OPTI] Reduce a bit access to mat

--- src/score.rs
+++ src/score.rs
@@ -15,9 +15,8 @@ pub fn test_matrix_pattern_and_line<const N: usize>(mat: &[[bool; N]; N]) -> u32
     return matrix_pattern_and_line(mat);
 }
 
-const fn matrix_score_global<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
+const fn matrix_score_squares<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
     let mut square_score = 0;
-    let mut dark_modules = 0u32;
 
     let mut i = 0;
     while i < N - 1 {
@@ -33,8 +32,6 @@ const fn matrix_score_global<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
             buffer |= (mat[i + 0][j + 1] as u8) << 2;
             buffer |= (mat[i + 1][j + 1] as u8) << 3;
 
-            dark_modules += (buffer as u32) & 1;
-
             if buffer == 0b1111 || buffer == 0b0000 {
                 square_score += 3;
             }
@@ -44,31 +41,7 @@ const fn matrix_score_global<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
         i += 1;
     }
 
-    let mut i = 0;
-    while i < N {
-        if mat[i][N - 1] {
-            dark_modules += 1;
-        }
-        if mat[N - 1][i] {
-            dark_modules += 1;
-        }
-        i += 1;
-    }
-
-    let percent = (dark_modules as usize * 100) / (N * N);
-    let mut lower_bound = (percent - (percent % 5)) as i8;
-    let mut higher_bound = (percent + (5 - percent % 5)) as i8;
-
-    lower_bound = (lower_bound - 50).abs();
-    higher_bound = (higher_bound - 50).abs();
-
-    let dark_score = if lower_bound < higher_bound {
-        lower_bound * 2
-    } else {
-        higher_bound * 2
-    } as u32;
-
-    return square_score + dark_score;
+    return square_score;
 }
 
 const fn score_trailing(buffer: u16, buffer_size: u32) -> u32 {
@@ -155,11 +128,18 @@ const fn matrix_pattern_and_line<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
     let mut buffer_col = [false; N];
     let mut score = 0;
 
+    let mut dark_modules = 0;
+
     let mut i = 0;
     while i < N {
         let mut j = 0;
         while j < N {
-            buffer_col[j] = mat[j][i];
+            let tmp = mat[j][i];
+            buffer_col[j] = tmp;
+            if tmp {
+                dark_modules += 1;
+            }
+
             j += 1;
         }
 
@@ -169,10 +149,23 @@ const fn matrix_pattern_and_line<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
         i += 1;
     }
 
-    return score;
+    let percent = (dark_modules as usize * 100) / (N * N);
+    let mut lower_bound = (percent - (percent % 5)) as i8;
+    let mut higher_bound = (percent + (5 - percent % 5)) as i8;
+
+    lower_bound = (lower_bound - 50).abs();
+    higher_bound = (higher_bound - 50).abs();
+
+    let dark_score = if lower_bound < higher_bound {
+        lower_bound * 2
+    } else {
+        higher_bound * 2
+    } as u32;
+
+    return score + dark_score;
 }
 
 /// Adds every score together
 pub const fn matrix_score<const N: usize>(mat: &[[bool; N]; N]) -> u32 {
-    return matrix_score_global(mat) + matrix_pattern_and_line(mat);
+    return matrix_score_squares(mat) + matrix_pattern_and_line(mat);
 }
