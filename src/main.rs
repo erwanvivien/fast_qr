189f208acefb9d4bf2777d3d57bfba1fae9913d5
erwan.vivien <erwan.vivien@epita.fr>
2021-10-19 01:39:00 +0200

    [FEATURE] Can now specify mask

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,12 @@ mod version;
 /// Still useless, only test purposes for now.
 fn main() {
     const CONTENT: &str = "https://vahan.dev/this_url_doesnt_exist";
+    const MASK: Option<usize> = None;
+    const VERSION: Option<version::Version> = None;
+    const LEVEL: Option<vecl::ECL> = Some(vecl::ECL::H);
+
     const QRCODE: Option<qrcode::QRCode> =
-        qrcode::QRCode::new(CONTENT.as_bytes(), vecl::ECL::H, None);
+        qrcode::QRCode::new(CONTENT.as_bytes(), LEVEL, VERSION, MASK);
 
     if let Some(q) = QRCODE {
         q.print();
