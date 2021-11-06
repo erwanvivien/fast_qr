0aa7c310f391bdad4e1ac4833a17062c1020b1a7
Vahan Boghossian <vahanboghoss@gmail.com>
2021-11-06 18:06:34 +0100

    [REFACTO] Moved modules to lib.rs file

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests;
+
+mod bitstring;
+mod datamasking;
+mod default;
+pub mod ecl;
+mod encode;
+mod hardcode;
+mod helpers;
+mod placement;
+mod polynomials;
+pub mod qrcode;
+mod score;
+pub mod version;
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-#[cfg(test)]
-mod tests;
-
-mod bitstring;
-mod datamasking;
-mod default;
-mod ecl;
-mod encode;
-mod hardcode;
-mod helpers;
-mod placement;
-mod polynomials;
-mod qrcode;
-mod score;
-mod version;
+use qrgen::ecl::ECL;
+use qrgen::qrcode::QRCode;
+use qrgen::version::Version;
 
 /// Still useless, only test purposes for now.
 fn main() {
     const CONTENT: &str = "https://vahan.dev/";
     const MASK: Option<usize> = None;
-    const VERSION: Option<version::Version> = None;
-    const LEVEL: Option<ecl::ECL> = Some(ecl::ECL::H);
+    const VERSION: Option<Version> = None;
+    const LEVEL: Option<ECL> = Some(ECL::H);
 
-    const QRCODE: Option<qrcode::QRCode> =
-        qrcode::QRCode::new(CONTENT.as_bytes(), LEVEL, VERSION, MASK);
+    const QRCODE: Option<QRCode> = QRCode::new(CONTENT.as_bytes(), LEVEL, VERSION, MASK);
 
     if let Some(q) = QRCODE {
         q.print();
