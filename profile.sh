6f8a21ac34038917e7df270c86593581ea305e57
erwan.vivien <erwan.vivien@epita.fr>
2022-04-02 15:17:28 +0200

    fix: shorter arg parsing

--- profile.sh
+++ profile.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-cargo build --release
-
 args="${*:-https://vahan.dev/}"
-
 echo "$args"
 
-exit
+cargo build --release
 
 echo "$args" | perf record --call-graph=dwarf target/release/qrgen && \
 perf script -F +pid > /tmp/test.perf && \
