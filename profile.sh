20968a82220901005ca4d52bc60d95daad234274
erwan.vivien <erwan.vivien@epita.fr>
2022-04-11 09:49:10 +0200

    fix: removing reference to qrgen

--- profile.sh
+++ profile.sh
@@ -5,7 +5,7 @@ echo "$args"
 
 cargo build --release
 
-echo "$args" | perf record --call-graph=dwarf target/release/qrgen && \
+echo "$args" | perf record --call-graph=dwarf target/release/fast_qr && \
 perf script -F +pid > /tmp/test.perf && \
 
 echo "View results on: https://profiler.firefox.com/"
