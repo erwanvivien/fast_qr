cae0e32b31128a1b1cbcd0903c95ba3d8562624f
Erwan VIVIEN <erwan.vivien@epita.fr>
2022-07-22 14:45:38 +0200

    fix: removing unused #cfg test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,18 +1,10 @@
-#[cfg(test)]
 mod bitstring;
-#[cfg(test)]
 mod datamasking;
 mod default;
-#[cfg(test)]
 mod encode;
 mod error_correction;
-#[cfg(test)]
-mod hardcode;
-#[cfg(test)]
 mod polynomials;
-#[cfg(test)]
 mod score;
 mod structure;
 mod svg;
-#[cfg(test)]
 mod version;
