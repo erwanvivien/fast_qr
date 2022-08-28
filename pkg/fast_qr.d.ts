53fa6530ae182144105a73fc0c6a8a1d1ad02b9d
erwanvivien <44021072+erwanvivien@users.noreply.github.com>
2022-08-28 11:27:15 +0200

    Wasm SVG feature (#5)
    * feat: Showcase NPM fast_qr in README
    
    * fix: remove unused / unusable function
    
    * feat: add pkg to git
    
    * feat: first version
    
    * fix: options is optionnal
    
    * fix: change type & main
    
    * fix: param options is optionnal & return string
    
    * fix: added type for Options
    
    * fix: doesnt trust input
    
    * fix: global options to help JavaScript find type
    
    * fix: duplicate export
    
    * fix: made options Partial (all optionnal)
    
    * fix: Circle was using x,x instead of x,y
    
    * fix: default color were wrong
    
    * fix: added optionnal params based on shape
    
    * fix: params is now called

---
+++ pkg/fast_qr.d.ts
@@ -0,0 +1,81 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* Generate a QR code from a string. All parameters are automatically set.
+* @param {string} content
+* @returns {Uint8Array}
+*/
+export function qr(content: string): Uint8Array;
+
+type Shape =
+  | "Square"
+  | "Circle"
+  | "RoundedSquare"
+  | "Vertical"
+  | "Horizontal"
+  | "Diamond"
+;
+
+/**
+ * Optionnal params to generate an SVG
+ */
+export type QrSvgOptions = {
+  /**
+   * Shape is for every module (pixel)
+   * Default: "Square"
+   */
+  shape: Shape;
+  /**
+   * A QRCode needs margins of at least 4 in order to be read.
+   * If QRCode is not readable, try increasing margins
+   * Default: 4
+   */
+  margin: number;
+  /**
+   * The color of the background of the SVG, any string will be used as if valid
+   * Default: "#FFF"
+   */
+  background_color: string;
+  /**
+   * The color of every module (pixel), any string will be used as if valid
+   * Default: "#000"
+   */
+  module_color: string;
+};
+
+/**
+* Generate a QR code from a string. All parameters are automatically set.
+* @param {string} content
+* @returns {Uint8Array}
+*/
+export function qr_svg(content: string, options?: Partial<QrSvgOptions>): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly qr: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/index.js
@@ -0,0 +1,127 @@
+import init, { initSync, qr } from "./fast_qr.js";
+
+export { initSync, qr };
+export default init;
+
+const shapes = [
+  "Square",
+  "Circle",
+  "RoundedSquare",
+  "Vertical",
+  "Horizontal",
+  "Diamond",
+];
+// as const;
+
+const DEFAULT_OPTIONS = {
+  background_color: "#FFF",
+  margin: 4,
+  module_color: "#000",
+  shape: "Square",
+};
+
+const shapeParams =
+  // : {
+  //   [K in Shape]: (p: { x: number; y: number }, margin: number) => string;
+  // }
+  {
+    Circle: (_) => "",
+    Square: (_) => "",
+    RoundedSquare: (options = { ...DEFAULT_OPTIONS }) =>
+      `stroke-width='.3' stroke-linejoin='round' stroke='${options.module_color}'`,
+    Diamond: (_) => "",
+    Horizontal: (_) => "",
+    Vertical: (_) => "",
+  };
+
+// const updateOpt = (o?: Partial<QrSvgOptions>) => {
+const updateOpt = (o) => {
+  // const opt: QrSvgOptions = {
+  const opt = {
+    ...DEFAULT_OPTIONS,
+  };
+
+  if (!o) return opt;
+
+  if (o.module_color && typeof o.module_color === "string") {
+    opt.module_color = o.module_color;
+  }
+  if (o.margin && typeof o.margin === "number" && o.margin >= 0) {
+    opt.margin = o.margin;
+  }
+  if (o.background_color && typeof o.background_color === "string") {
+    opt.background_color = o.background_color;
+  }
+  if (o.shape && typeof o.shape === "string" && shapes.includes(o.shape)) {
+    opt.shape = o.shape;
+  }
+
+  return opt;
+};
+
+const fmtShape =
+  // : {
+  //   [K in Shape]: (p: { x: number; y: number }, margin: number) => string;
+  // }
+  {
+    Circle: (p, margin) =>
+      `M${p.x + margin + 1},${p.y + margin + 0.5}a.5,.5 0 1,1 0,-.1`,
+    Diamond: (p, margin) =>
+      `M${p.x + margin}.5,${p.y + margin}l.5,.5l-.5,.5l-.5,-.5z`,
+    Horizontal: (p, margin) => `M${p.x + margin}.1,${p.y + margin}h1v.8h-1`,
+    RoundedSquare: (p, margin) =>
+      `M${p.x + margin}.2,${p.y + margin}.2 ${p.x + margin}.8,${
+        p.y + margin
+      }.2` +
+      ` ${p.x + margin}.8,${p.y + margin}.8 ${p.x + margin}.2,${
+        p.y + margin
+      }.8z`,
+    Square: (p, margin) => `M${p.x + margin},${p.y + margin}h1v1h-1`,
+    Vertical: (p, margin) => `M${p.x + margin},${p.y + margin}.1h.8v1h-.8`,
+  };
+
+// const qr_svg = (content: string, options?: Partial<QrSvgOptions>) => {
+function qr_svg(content = "", options = { ...DEFAULT_OPTIONS }) {
+  if (typeof content !== "string") {
+    return "";
+  }
+
+  const opt = updateOpt(options);
+
+  const QR = qr(content);
+  const QRsize = Math.sqrt(QR.length);
+
+  // const pos: { x: number, y: number }[] = [];
+  const pos = [];
+  for (let i = 0; i < QRsize; i += 1) {
+    for (let j = 0; j < QRsize; j += 1) {
+      if (QR[i * QRsize + j] !== 1) continue;
+
+      pos.push({
+        x: j,
+        y: i,
+      });
+    }
+  }
+
+  const { background_color, margin, module_color, shape } = opt;
+
+  /// M places the cursor and h1v1h-1 draws a square
+  const svgPath = pos.map((p) => fmtShape[shape](p, margin)).join("");
+
+  const size = QRsize + margin * 2;
+  const params = shapeParams[shape](opt);
+  const svg = [
+    `<svg
+  viewBox='0 0 ${size} ${size}'
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink">`,
+    `<path d='M0,0h${size}v${size}h-${size}' fill='${background_color}' />`,
+    `<path d='${svgPath}' fill='${module_color}' opacity='1' ${params} />`,
+    `</svg>`,
+  ];
+
+  return svg.join("");
+}
+
+export { qr_svg };
