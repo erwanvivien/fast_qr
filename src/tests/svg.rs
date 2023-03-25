d57993d84f0882e415c3c7692a3c323f44185312
m0n0096 <max.nachlinger@walmart.com>
2023-03-25 10:20:30 -0700

    chore: remove base64 dependency and png asset

--- src/tests/svg.rs
+++ src/tests/svg.rs
@@ -2,13 +2,11 @@
 use crate::convert::svg::SvgBuilder;
 use crate::convert::Builder;
 use crate::{QRBuilder, ECL};
-use base64::{engine::general_purpose, Engine as _};
 
 #[cfg(feature = "svg")]
 #[test]
 fn it_embeds_an_image_via_data_uri() {
-    let image_bytes = std::fs::read("assets/red_square.png").unwrap();
-    let image_base64 = general_purpose::STANDARD.encode(&image_bytes);
+    let image_base64 = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAFUlEQVR4AWP4oyVDEhrGGkY1jGoAABACQhA+7XDPAAAAAElFTkSuQmCC";
     let data_uri = format!("data:image/png;base64,{image_base64}");
 
     let qrcode = QRBuilder::new("https://example.com/")
