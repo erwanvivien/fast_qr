60174c1d34cf4d579eb473ef43f6401db0423fb3
Erwan Vivien <vivien.erwan@gmail.com>
2023-12-29 21:28:25 +0100

    fix: SVG and images were inverted
    This is because I called commands with (x, y) instead of (y, x)
    Fixes #47

--- src/tests/svg.rs
+++ src/tests/svg.rs
@@ -20,3 +20,32 @@ fn it_embeds_an_image_via_data_uri() {
     let expected_href = format!(r#"href="{data_uri}""#);
     assert!(svg.contains(&expected_href));
 }
+
+#[cfg(feature = "svg")]
+#[test]
+fn check_svg_is_not_inverted() {
+    use crate::convert::svg::SvgBuilder;
+    use crate::convert::Builder;
+    use crate::{QRBuilder, Version, ECL};
+
+    let qrcode = QRBuilder::new("Test")
+        .ecl(ECL::M)
+        .version(Version::V01)
+        .build()
+        .unwrap();
+
+    const MARGIN: usize = 4;
+    let svg = SvgBuilder::default().margin(MARGIN).to_str(&qrcode);
+
+    let size = qrcode.size;
+    for y in 0..size {
+        for x in 0..size {
+            let index = y * size + x;
+            let expected = qrcode.data[index];
+            if expected.value() {
+                let expected = format!(r#"M{x},{y}"#, x = x + MARGIN, y = y + MARGIN);
+                assert!(svg.contains(&expected));
+            }
+        }
+    }
+}
